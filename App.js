cb430f1a3952a54a325f8d8e788a31d380105ca8
Hugoneytor <halvarez0@ucol.mx>
2021-12-09 14:36:31 -0600

    Json complete

--- App.js
+++ App.js
@@ -1,18 +1,39 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
 
 const App = () => {
-  return (
-    <View >
-      <Text style={styles.contenedor}>Hola Mundo</Text>
-    </View>
-  )
+    return (
+        <View style={styles.contenedor}>
+            <View style={styles.contenedorUno}>
+                <Text style={styles.texto}>App Inmobiliaria</Text>
+            </View>
+            <View style={styles.contenedorDos}>
+                <Text style={styles.texto}>XD</Text>
+            </View>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  contenedor:{
-    fontSize: 50
-  }
+    texto: {
+        fontSize: 50,
+        textAlign: 'center'
+    },
+    contenedor:{
+      flex:1,
+    },
+    contenedorUno:{
+      flex:1,
+      backgroundColor: 'green',
+      justifyContent: 'center',
+
+    },
+    contenedorDos:{
+      flex:1,
+      backgroundColor: 'red',
+      justifyContent: 'center',
+      
+    }
 })
 
 export default App
