841a23d81e6dd73652beb418bc49927041840ed8
Hugoneytor <halvarez0@ucol.mx>
2021-12-10 17:20:32 -0600

    Design Added

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
 /* eslint-disable react/prop-types */
 import React from "react";
-import {Text, View, FlatList} from "react-native";
+import {Text, View, FlatList, ImageBackground, StyleSheet} from "react-native";
 import hoteles from "./src/data.json";
 
-const Item = ({ name }) => (
-    <View>
-      <Text>{name}</Text>
+const Item = ({ id, name, address, rooms, bathrooms, feets, price, ratings, img }) => (
+    <View style={styles.container}>
+
+        <ImageBackground style={styles.contenedorUno} source={{uri:img}}>
+          <Text>{ratings}</Text>
+        </ImageBackground>
+
+      <View style={styles.contenedorDos}>
+        <Text style={styles.Text}>{id}</Text>
+        <Text>{name}</Text>
+        <Text>{address}</Text>
+        <Text>{rooms}</Text>
+        <Text>{bathrooms}</Text>
+        <Text>{feets}</Text>
+        <Text>{price}</Text>
+      </View>
     </View>
   );
 
 const App = () => {
-
     const renderItem = ({ item }) => (
-        <Item name={item.name} />
+        <Item {...item} />
       );
-
     return (
         <View>
             <FlatList
@@ -25,5 +36,26 @@ const App = () => {
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+  container:{
+    display: "flex",
+    flexDirection: "row",
+    flex: 1,
+    padding: 20
+  },  
+  Text:{
+    color: "red"
+  },
+  contenedorUno:{
+    flex: 1.5,
+    
+    borderRadius: 30,
+    overflow: "hidden",
+  },
+  contenedorDos:{
+    flex: 2,
+  }
+});
 
 export default App;
